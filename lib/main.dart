5d2f02312ae6ff060f0061ac0d5b137978658c49
khdr32854-glitch <khdr32854@gmail.com>
2026-08-08 01:56:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
-lib/main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mosul Scanner'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'Mosul Scanner App Ready!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
